10d6254ea7a077b67caa2a41b37a5d79b0375e5e
longobucco <longobuccofbc@gmail.com>
2025-01-27 18:59:33 -0300

    fix class name  ConfigBeans

--- src/main/java/com/buccodev/app_register/infrastructure/config/ConfigBeans.java
+++ src/main/java/com/buccodev/app_register/infrastructure/config/ConfigBeans.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class configBeans {
+public class ConfigBeans {
 
     @Bean
     CreateUserGateway createUserGateway(CreateUserUsecase usecase){
